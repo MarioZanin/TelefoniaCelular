c9435bba003df244f7b0a53d0bdb791cce638210
Gabriel Tasso <91082691+GabrielTasso@users.noreply.github.com>
2023-10-27 19:55:02 -0300

    Prepago test

--- src/PrePago.java
+++ src/PrePago.java
@@ -9,21 +9,29 @@ class PrePago {
 
     // Construtor
     public PrePago(long cpf, String nome, int numero) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.numero = numero;
         
     }
 
     // Método para realizar uma recarga
     public void recarga(Date data, float valor) {
+        this.data = data;
+        this.valor = valor;
         
     }
 
     // Método para fazer uma chamada
     public float fazerChamada(Date data, int duracao) {
+        return this.data = data;
+        return this.duracao = duracao;
         
     }
 
     // Método para imprimir a fatura de um determinado mês
     public void imprimirFatura(int mes) {
+        this.mes = mes;
        
     }
 }
