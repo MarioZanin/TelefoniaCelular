b59c71f9eb4d824066b6d7e279e9a209b4998727
Gabriel Tasso <91082691+GabrielTasso@users.noreply.github.com>
2023-10-28 18:33:25 -0300

    ligando os extends e as  funcões do codig

--- src/PosPago.java
+++ src/PosPago.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-class PosPago {
+class PosPago extends Assinante {
     private long cpf;
     private String nome;
     private int numero;
@@ -8,16 +8,27 @@ class PosPago {
    
     // Construtor
     public PosPago(long cpf, String nome, int numero, float assinatura) {
-        
+        super(cpf, nome, numero, assinatura)
+        this.assinatura = assinatura;        
+    }
+
+    public float getassinatura(float assinatura){
+        return assinatura;
+    }
+
+    public void setassinatura(){
+        this.assinatura = assinatura;
     }
 
     // Método para fazer uma chamada
     public void fazerChamada(Date data, int duracao) {
+        this.data = data;
+        this.duracao = duracao;
         
     }
 
     // Método para imprimir a fatura de um determinado mês
     public void imprimirFatura(int mes) {
-        
+        this.mes = mes;        
     }
 }
